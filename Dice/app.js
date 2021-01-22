2893315a26197b712f1ada4bab27857eaff037dc
ayushjain <ayush.071998@gmail.com>
2021-01-22 12:54:24 +0530

    Add files via upload

---
+++ Dice/app.js
@@ -0,0 +1,21 @@
+var randomNumber1=Math.floor(Math.random() * 6)+1;
+var randomdice1="images/"+"dice"+randomNumber1+".png"
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute('src',randomdice1)
+
+var randomNumber2=Math.floor(Math.random() * 6)+1;
+var randomdice2="images/"+"dice"+randomNumber2+".png"
+var image1=document.querySelectorAll("img")[1];
+image1.setAttribute('src',randomdice2)
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector('h1').innerHTML='Play1 Wins!🚩';
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector('h1').innerHTML='Play2 Wins!🚩';
+
+}
+else{
+    document.querySelector('h1').innerHTML='DRAW!😳';
+}
